506903af73d3a7db81ccb119840f9f2931b3b84a
Amy-038 <amy.sirma@student.moringaschool.com>
2024-02-03 18:19:00 +0300

    Completed code-along

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+
+/*function Toggle() {
+  const [ count, setCount ] = useState('OFF')
+
+  function handleClick(){
+    if(count === "OFF"){
+      setCount('ON')
+    } else if (count === "ON"){
+      setCount("OFF")
+    }
+    
+  }
+  return <button onClick={handleClick}>{count}</button>;
+}*/
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false);
+
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
+
+  const color = isOn ? "red" : "white";
+
+  return (
+    <button style={{ background: color }} onClick={handleClick}>
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
 }
 
 export default Toggle;
